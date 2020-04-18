b5a292d57a6b315cea1c2a13984cdf94ac703438
Saem Ghani <saem@system76-pc.localdomain>
2020-04-17 21:20:17 -0700

    PbrLight now hides shader type parameter
    This fixes a bug missed in the last commit that broke the lights sample.
    
    Settings things up to consolidate state and collapse classes.

--- h3d/scene/pbr/DirLight.hx
+++ h3d/scene/pbr/DirLight.hx
@@ -1,11 +1,10 @@
 package h3d.scene.pbr;
 
-class DirLight extends Light<h3d.shader.pbr.Light.DirLight> {
+class DirLight extends Light.PbrLight<h3d.shader.pbr.Light.DirLight> {
 
 	@:allow(h3d.scene.Object.createPbrDirLight)
 	private function new(?dir: h3d.Vector, ?parent) {
-		shadows = new h3d.pass.DirShadowMap(this);
-		super(new h3d.shader.pbr.Light.DirLight() ,parent);
+		super(new h3d.shader.pbr.Light.DirLight(), new h3d.pass.DirShadowMap(this) ,parent);
 		if( dir != null ) setDirection(dir);
 	}
 
