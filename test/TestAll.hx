08b236a9c6b21a18aaabd4c0db4227a24c9637ce
Saem Ghani <saemghani+github@gmail.com>
2020-04-26 13:04:20 -0700

    Introduce src and test directories
    Also added a PoC test and test.hxml

---
+++ test/TestAll.hx
@@ -0,0 +1,9 @@
+package;
+
+import htst.RandomTest;
+
+class TestAll {
+    public static function main() {
+        utest.UTest.run([new RandomTest()]);
+    }
+}
---
+++ test/htst/RandomTest.hx
@@ -0,0 +1,7 @@
+package htst;
+
+class RandomTest extends utest.Test {
+    function testNothing() {
+        utest.Assert.isTrue(true, "farts");
+    }
+}
