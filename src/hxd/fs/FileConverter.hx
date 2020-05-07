17de3a772307bf9bcb54804454543cb40ea0e833
Matt Chudleigh <matt.chudleigh@gmail.com>
2020-05-07 07:21:21 -0700

    Support loading GLB files

--- src/hxd/fs/FileConverter.hx
+++ src/hxd/fs/FileConverter.hx
@@ -41,9 +41,10 @@ class FileConverter {
 		// this is the default converts config, it can be override in per-directory props.json
 		defaultConfig = makeConfig({
 			"fs.convert" : {
-				"fbx"  : { "convert" : "hmd", "priority" : -1 },
-				"gltf" : { "convert" : "hmd", "priority" : -1 },
-				"obj" : { "convert" : "hmd", "priority" : -1 },
+				"fbx"  : { "convert" : "hmd" , "priority" : -1 },
+				"gltf" : { "convert" : "hmd" , "priority" : -1 },
+				"glb"  : { "convert" : "hmd" , "priority" : -1 },
+				"obj"  : { "convert" : "hmd" , "priority" : -1 },
 				"fnt"  : { "convert" : "bfnt", "priority" : -1 }
 			}
 		});
