16498cc68802c729f2819bd9b5001b6a26fa9986
Saem Ghani <saemghani+github@gmail.com>
2020-05-24 13:03:24 -0700

    Fix AdjustColor sample so it works in Chrome
    The h2d.Bitmap API's set pixel is very expensive on browsers. Chrome
    experiences this the most severely.
    
    Used the lock/unlock API to bulk up the changes.

--- samples/AdjustColor.hx
+++ samples/AdjustColor.hx
@@ -6,23 +6,25 @@ class AdjustColor extends SampleApp {
 	var bright = 0.;
 	var contrast = 0.;
 
-	var bmps : Array<h2d.Bitmap>;
+	var bmps : Array<h2d.Bitmap> = [];
 
 	override function init() {
 		super.init();
 
 		engine.backgroundColor = 0x404040;
 
-		bmps = [];
 		for( i in 0...4 ) {
 			var gradient = new hxd.BitmapData(256, 256);
 			var red = (i + 1) & 1;
 			var green = ((i + 1) >> 1) & 1;
 			var blue = (i + 1) >> 2;
-			for( x in 0...gradient.width )
-				for( y in 0...gradient.height )
+			gradient.lock();
+			for( x in 0...gradient.width ) {
+				for( y in 0...gradient.height ) {
 					gradient.setPixel(x,y, 0xFF000000 | ((x << 16) * red) | ((y << 8) * green) | (((x + y) >> 1) * blue));
-
+				}
+			}
+			gradient.unlock();
 			var bmp = new h2d.Bitmap(h2d.Tile.fromBitmap(gradient), s2d);
 			bmp.x = 50 + (i&1) * 270;
 			bmp.y = 100 + (i >> 1) * 270;
