af33473d0c73c3e9910ecb2cdc9f55cda013daa4
projectitis <32183808+projectitis@users.noreply.github.com>
2020-06-01 23:24:48 +1200

    Added BDF font parser (#794)

--- src/hxd/res/Config.hx
+++ src/hxd/res/Config.hx
@@ -20,6 +20,7 @@ class Config {
 		"fbx,hmd,obj,gltf,glb" => "hxd.res.Model",
 		"ttf" => "hxd.res.Font",
 		"fnt" => "hxd.res.BitmapFont",
+		"bdf" => "hxd.res.BDFFont",
 		"wav,mp3,ogg" => "hxd.res.Sound",
 		"tmx" => "hxd.res.TiledMap",
 		"atlas" => "hxd.res.Atlas",
